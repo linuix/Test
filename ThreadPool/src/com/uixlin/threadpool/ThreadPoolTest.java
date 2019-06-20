694117ba989b7d10f277b9c994d5a4e0b0271252
link <uixlin@gmail.com>
2019-06-20 23:54:19 +0800

    线程池

---
+++ ThreadPool/src/com/uixlin/threadpool/ThreadPoolTest.java
@@ -0,0 +1,18 @@
+package com.uixlin.threadpool;
+
+public class ThreadPoolTest {
+
+    public static void main(String[] arg){
+        ThreadExcutor excutor = new ThreadExcutor(3);
+        for (int i = 0; i < 10; i++) {
+            excutor.exec(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("线程 " + Thread.currentThread().getName() + " 在帮我干活");
+                }
+            });
+        }
+        excutor.shutdown();
+    }
+
+}
